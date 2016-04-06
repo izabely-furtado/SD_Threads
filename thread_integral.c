dfb72e6961cb97083f728f8ae6afce770c15fd5d
Izabely Furtado <izabelyfurtado1@gmail.com>
2016-04-05 21:27:38 -0300

    2.3.3
    bli

--- thread_integral.c
+++ thread_integral.c
@@ -1,115 +1,115 @@
-/* ====================================================================
- * File:   thread_integral.c
- * Author: 20121bsi0040 - Izabely Almeida Furtado Corrêa
- * Descrição: Este programa calcula a integral através de N threads
- * N = numero de threads do computador
- * Created on 30 de Março de 2016, 09:03
- * Para compilar:
- *     gcc thread_integral.c -o thread_integral -lpthread -lm -Wall
- * Para executar:
- *     time ./thread_integral 0.0 1.0 200000000
- * ====================================================================*/
+    /* ====================================================================
+     * File:   thread_integral.c
+     * Author: 20121bsi0040 - Izabely Almeida Furtado Corrêa
+     * Descrição: Este programa calcula a integral através de N threads
+     * N = numero de threads do computador
+     * Created on 30 de Março de 2016, 09:03
+     * Para compilar:
+     *     gcc thread_integral.c -o thread_integral -lpthread -lm -Wall
+     * Para executar:
+     *     time ./thread_integral 0.0 1.0 200000000
+     * ====================================================================*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <sys/sysinfo.h>
-#include <math.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <pthread.h>
+    #include <unistd.h>
+    #include <sys/sysinfo.h>
+    #include <math.h>
 
 
-double integral = 0.0;
-pthread_mutex_t lock;
-pthread_t threads[];
+    double integral = 0.0;
+    pthread_mutex_t lock;
+    pthread_t threads[];
 
-//função que calcula a area de um trapézio
-double AreaTrapezio(double dx, double h1, double h2){
-    double area;
-    area = dx * (h1+h2)/2;
-    return area;
-}
-
-//função qualquer f(x)
-double f(double x){
-    return (4*sqrt(1-x*x));
-}
-
-//criando limite
-typedef struct limite
-{
-    double inferior;
-    double superior;
-    long n;
-}Limite;
-
-Limite CriaLim(double inferior, double superior, long numero){
-    Limite *l = (Limite *) malloc(sizeof(Limite));
-    l->inferior = inferior;
-    l->superior = superior;
-    l->n = numero;
-    return l;
-}
-
-//calcula a area da integral da função f(x)
-double CalculaArea(double a, double b, int N){
-    int i;
-    double area = 0.0, dx, x1, x2, f1, f2;
-    for(i = 0; i<N; i++){
-        x1 = a + dx * i;
-        x2 = a + dx * (i + 1);
-        f1 = f(x1);
-        f2 = f(x2);
-        area += AreaTrapezio(dx, f1, f2);
+    //função que calcula a area de um trapézio
+    double AreaTrapezio(double dx, double h1, double h2){
+        double area;
+        area = dx * (h1+h2)/2;
+        return area;
     }
-    return area;
-}
 
-//ponteiro pra função ... 
-//dividindo f(x) para ser calculada por partes... (estilo Jack)
-void *ThreadCalculaArea(Limite *lim){
-    double area2 = CalculaArea(lim->inferior, lim->superior, lim->n);
-    
-    //controlando area que pode haver condição de corrida
-    pthread_mutex_lock(&lock);
-    integral += area2; 
-    pthread_mutex_unlock(&lock);
-
-    return NULL;
-}
-
-//criando threads
-void CriaThread(int quantThreads){
-    int i, k;
-    Limite* l;
-    int nProc = get_nprocs();
-    //criando vetor de threads
-    threads[quantThreads] = (pthread_t *) malloc(quantThreads*sizeof(pthread_t));
-    
-    //dando a cada thread um objetivo
-    for(i=0; i<quantThreads; i++) {
-        l = CriaLim((1/quantThreads)*i - (1/quantThreads), (1/quantThreads)*i);
-        pthread_create(&(threads[i]), NULL, &ThreadCalculaArea, &l);
+    //função qualquer f(x)
+    double f(double x){
+        return (4*sqrt(1-x*x));
     }
-    //mandando bala
-    for(k=0; k<quantThreads; k++) {
-        pthread_join(threads[k], NULL);
-    }
-}
 
-
-int main(int argc, char** argv) {
-    int nProc = get_nprocs();
-    
-    long numero = 25000000;
-    
-    if (pthread_mutex_init(&lock, NULL) != 0)
+    //criando limite
+    typedef struct limite
     {
-        printf("\n mutex init failed\n");
-        return 1;
+        double inferior;
+        double superior;
+        long n;
+    }Limite;
+
+    Limite CriaLim(double inferior, double superior, long numero){
+        Limite *l = (Limite *) malloc(sizeof(Limite));
+        l->inferior = inferior;
+        l->superior = superior;
+        l->n = numero;
+        return l;
     }
-    CriaThread(nProc);
-    printf("A area da curva é \n%.12lf\n", integral);
-    pthread_mutex_destroy(&lock);
-    return (EXIT_SUCCESS);
-}
+
+    //calcula a area da integral da função f(x)
+    double CalculaArea(double a, double b, int N){
+        int i;
+        double area = 0.0, dx, x1, x2, f1, f2;
+        for(i = 0; i<N; i++){
+            x1 = a + dx * i;
+            x2 = a + dx * (i + 1);
+            f1 = f(x1);
+            f2 = f(x2);
+            area += AreaTrapezio(dx, f1, f2);
+        }
+        return area;
+    }
+
+    //ponteiro pra função ... 
+    //dividindo f(x) para ser calculada por partes... (estilo Jack)
+    void *ThreadCalculaArea(Limite *lim){
+        double area2 = CalculaArea(lim->inferior, lim->superior, lim->n);
+
+        //controlando area que pode haver condição de corrida
+        pthread_mutex_lock(&lock);
+        integral += area2; 
+        pthread_mutex_unlock(&lock);
+
+        return NULL;
+    }
+
+    //criando threads
+    void CriaThread(int quantThreads, long divisoes){
+        int i, k;
+        Limite* l;
+        int nProc = get_nprocs();
+        //criando vetor de threads
+        threads[quantThreads] = (pthread_t *) malloc(quantThreads*sizeof(pthread_t));
+
+        //dando a cada thread um objetivo
+        for(i=0; i<quantThreads; i++) {
+            l = CriaLim((1/quantThreads)*i - (1/quantThreads), (1/quantThreads)*i, divisoes);
+            pthread_create(&(threads[i]), NULL, &ThreadCalculaArea, &l);
+        }
+        //mandando bala
+        for(k=0; k<quantThreads; k++) {
+            pthread_join(threads[k], NULL);
+        }
+    }
+
+
+    int main(int argc, char** argv) {
+        int nProc = get_nprocs();
+
+        long numero = 25000000;
+
+        if (pthread_mutex_init(&lock, NULL) != 0)
+        {
+            printf("\n mutex init failed\n");
+            return 1;
+        }
+        CriaThread(nProc, numero);
+        printf("A area da curva é \n%.12lf\n", integral);
+        pthread_mutex_destroy(&lock);
+        return (EXIT_SUCCESS);
+    }
 
